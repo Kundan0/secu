7c4ba608a5fb91e2a8fe3fcbdacc4b9525c8513a
Kundan0 <074bct517.kundan@pcampus.edu.np>
2022-02-28 20:15:34 +0545

    38 depths used

--- ClassData.py
+++ ClassData.py
@@ -27,7 +27,7 @@ class myDataset(Dataset):
         depths=[(depth-torch.mean(depth))/(torch.std(depth)) for depth in depths]
         cropped_depths=[]
         for i in range(len(depths)):
-            track_=self.data[index]["track"][i*18]
+            track_=self.data[index]["track"][i]
             left,top,right,bottom=track_
             #print("not resized  bbox",left,top,right,bottom)
             left=round(left/self.width_ratio)
